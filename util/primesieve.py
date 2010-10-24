df013b8594afc31794105d3db33076688820bc40
Sameer <Sameer@Sameer-PC>
2010-10-24 00:28:37 -0400

    fix width

--- util/primesieve.py
+++ util/primesieve.py
@@ -1,9 +1,13 @@
 '''
 Prime Sieve
 
-This module uses the Sieve of Eratosthenes to generate primes in range 1 to N
-Original algorithm based on definition at: http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-Optimizations included from solution provided at: http://projecteuler.net/index.php?section=problems&id=3
+This module uses the Sieve of Eratosthenes 
+to generate primes in range 1 to N
+
+Original algorithm based on definition at: 
+http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+Optimizations included from solution provided at: 
+http://projecteuler.net/index.php?section=problems&id=3
 '''
 
 from math import ceil
@@ -34,23 +38,27 @@ def getPrimes(n):
 
         # By definition, any i with no factors is prime
         if sieve[i] == False:
-            # We can now set each multiple of i to true since it can longer be prime
-            # Sieving goes from candidate p^2 to sievebound with step p
-            # If the nth element has value 2i + 1, then to get the index of p^2:
+            # We can now set each multiple of i to true 
+            # since it can longer be prime
+            # Sieving goes from candidate p^2 to 
+            # sievebound with step p
+            # If the nth element has value 2i + 1, 
+            # then to get the index of p^2:
             # p to p^2 = (p^2 - 1)/2 
             #          = (4i^2 + 4i)/2 
             #          = 2i(i + 1)
             start = 2*i*(i+1)
             step = 2*i+1
             for j in xrange(start, sievebound, step):
-                # Set the index j to true to mark this number as having
+                # Set the index j to true to
+                # mark this number as having
                 # factors and thus not prime
                 sieve[j] = True
 
     # Since we ignore all even numbers, add 2 to our list
     primes = [2]
     
-    # All candidates with no factors are the prime numbers    
+    # All candidates with no factors are prime numbers    
     for i in xrange(0, sievebound+1, 1):
         if sieve[i] == False:
             primes.append(2*i + 1)            
